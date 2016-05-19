afd33852ef12d2c9832c499fb4e045bd82c9f19e
dkhamsing <dkhamsing@cirruspath.com>
2016-05-19 11:58:45 -0700

    [script] deploy debug

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -7,6 +7,11 @@ git checkout master
 git config user.name "CI"
 git config user.email "dkhamsing@users.noreply.github.com"
 
+echo add readme
 git add README.md
+
+echo commit
 git commit -m "[auto] [ci skip] Generate README"
+
+echo push
 git push --quiet "https://${GH_TOKEN}@github.com/matteocrippa/awesome-swift" master:master > /dev/null 2>&1
